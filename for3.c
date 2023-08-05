24d3b38ff197e5a13bee1b4969e68c2470282686
Sahilbelim <106684275+Sahilbelim@users.noreply.github.com>
2023-08-05 11:58:26 +0530

    for loop

---
+++ for3.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+void main()
+{
+    int row, col1, col2,temp,flash;
+    temp = 1;
+    flash = 5;
+
+    for (row = 1; row <= 5;row++)
+    {
+
+        for (col1 = 1; col1 <= temp; col1++)
+        {
+            printf(" ");
+        }
+        for (col2 = 1; col2 <= flash; col2++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+        temp++;
+        flash--;
+    }
+}
+
+
+// _*
+
+// _____*****
+
+// _____*****
+// _____*****
+// _____*****
+// _____*****
+// _____*****
+
+// _* * * * *
+// __* * * *
+// ___* * *
+// ____* *
+// _____*
---
+++ while8.c
@@ -0,0 +1,37 @@
+// 4] 1, 5, 12, 22, 35, 51, 70, 92, 117, 145, 176 ... 3000
+#include<stdio.h>
+void main()
+{
+    int num,num2;
+    num = 1;
+    num2 = 1;
+
+   
+    while(num<3000)
+   {
+       printf("%d ,", num);
+       num2 = num2 + 3;
+       num = num + num2;
+   }
+
+
+}
+// printf("%d ,", num);
+// num = num + num2;
+// num2 = num2 + 3;//4
+// printf("%d ,", num);
+// num2 = num2 + 3; // 7
+// num = num + num2;
+// printf("%d ,", num);
+// num2 = num + 3;
+// num = num + num2;//10
+
+// printf("%d ,", num);
+// num2 = num2 + 3;//13
+// num = num + num2;
+
+// printf("%d ,", num);
+// num2 = num2 + 3;//16
+// num = num + num2;
+
+// printf("%d ,", num);
